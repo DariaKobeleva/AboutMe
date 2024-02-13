dbb6bfab44ee3c4b7685bce322e25684f58fc3a7
KDaria <dkdva@mail.ru>
2024-02-13 18:13:45 +0100

    Add new views and models for user information display
    - Implement `User` and `Person` structures to encapsulate user data.
    - Create new UILabels in the view controller to present user information.
    - Bind user model properties to UILabels to display name, age, city, education, and bio.
    - Ensure data flow from the model to the views within the view controller's lifecycle.

---
+++ AboutMe/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  AboutMe
+//
+//  Created by Дарья Кобелева on 13.02.2024.
+//
+
+import Foundation
---
+++ AboutMe/ViewControllers/AboutMeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AboutMeViewController.swift
+//  AboutMe
+//
+//  Created by Дарья Кобелева on 13.02.2024.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
